1c25a6e81da34c15d4ad7c563021f972f31535b3
grantharper <grant.e.harper@gmail.com>
2018-05-09 20:01:10 -0400

    add logger to example parser

--- src/main/java/com/example/recipe/SimpleRecipeParserImpl.java
+++ src/main/java/com/example/recipe/SimpleRecipeParserImpl.java
@@ -1,10 +1,15 @@
 package com.example.recipe;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.grantharper.recipe.parser.RecipeParser;
 import org.grantharper.recipe.parser.RecipeParserAbstract;
 
 public class SimpleRecipeParserImpl extends RecipeParserAbstract implements RecipeParser
 {
+
+  private static final Logger logger = LogManager.getLogger(SimpleRecipeParserImpl.class);
+
   @Override
   protected void identifyLineIndexes()
   {
